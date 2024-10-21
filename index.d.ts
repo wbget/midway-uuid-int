ed7c930f3a3041087c09aa1302a8b476fd2a0e11
wbget <157354966@qq.com>
2024-10-21 18:25:17 +0800

    fix .d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,10 @@
 import '@midwayjs/redis';
-import { PowerPartial, ServiceFactoryConfigOption } from '@midwayjs/core';
 export * from './dist/index';
 
 declare module 'midway-uuid-int/dist/interface' {
   interface MidwayConfig {
-    uuidInt?: ServiceFactoryConfigOption<
-      PowerPartial<{
-        key?: string;
-      }>
-    >;
+    uuidInt?: {
+      key?: string;
+    };
   }
 }
